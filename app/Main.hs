3e417e2113bdb84e64ae46bf7d3c862c4ceb8f7d
Kevin Simon <syasnomis93@gmail.com>
2017-04-18 22:13:40 -0700

    Can parse atoms, strings, and numbers.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
 data LispVal = Atom String
              | List [LispVal]
@@ -9,10 +10,20 @@ data LispVal = Atom String
              | String String
              | Bool Bool
 
+nonQuote :: Parser Char
+nonQuote = noneOf "\\\""
+
+quote :: Parser Char 
+quote = do
+          char '\\'
+          q  <- oneOf "\\\""
+          return q
+
+
 parseString :: Parser LispVal
 parseString = do
                 char '"'
-                x <- many (noneOf "\"")
+                x <- many  (quote <|> nonQuote)
                 char '"'
                 return $ String x
 
@@ -26,6 +37,22 @@ parseAtom = do
                           "#f" -> Bool False
                           _    -> Atom atom
 
+parseNumber1 :: Parser LispVal
+parseNumber1 = liftM (Number . read) $ many1 digit
+
+parseNumber2 :: Parser LispVal
+parseNumber2 = (many1 digit) >>= \x -> (return . Number . read) x
+
+parseNumber :: Parser LispVal
+parseNumber = do
+                num <- many1 digit
+                (return . Number . read) num
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
@@ -33,7 +60,7 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
